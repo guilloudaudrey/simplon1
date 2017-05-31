b417395c8c6bc9bef79f295a246a06c49e03f679
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-05-31 17:04:56 +0200

    related

--- index.php
+++ index.php
@@ -113,64 +113,9 @@ include('header.php');
         <hr>
     </aside>
     <section class="grid">
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
-    </section>
-    <section class="grid">
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
-    </section>
-    <section class="grid">
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
-        <article class="gridart">
-            <img src="img/grid1.jpg" alt="prince" title="prins" />
-            <p class="gridname">Drank & Drugs</p>
-            <p class="gridsubtitle">sam de jong</p>
-            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
-        </article>
+    <?php
+  require('related.php');
+  ?>
     </section>
     <aside class="type">
         <p>unrelated</p>
---
+++ related.php
@@ -0,0 +1,58 @@
+      <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
+        <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
+        <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
+    </section>
+    <section class="grid">
+        <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
+        <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
+        <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
+    </section>
+    <section class="grid">
+        <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
+        <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
+        <article class="gridart">
+            <img src="img/grid1.jpg" alt="prince" title="prins" />
+            <p class="gridname">Drank & Drugs</p>
+            <p class="gridsubtitle">sam de jong</p>
+            <p class="gridtxt">This catchy tune with its wacky music video drives parents mad as 10 year olds sing along with the summer hit of 2015 . 15 Million views and counting!</p>
+        </article>
